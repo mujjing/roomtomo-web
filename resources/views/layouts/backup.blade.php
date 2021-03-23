a3cabfdc0d3a5eb968e81932ea050a50f009eee5
田知勲 <j-jihoon@winas.jp>
2021-03-24 00:09:18 +0900

    home화면

---
+++ resources/views/layouts/backup.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/571aadaefb.js"></script>
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>sumitomo</title>
+</head>
+<body>
+
+    <div class="wrapper">
+        <div class="sidebar">
+            <img src="../images/sumitomo_logo.png" alt="logo" class="logo_img">
+            <h2>sumitomo</h2>
+                <ul>
+                    <li><a href="#"><i class ="fas fa-home"></i>ホーム</a></li>
+                    <li><a href="#"><i class ="fas fa-address-card"></i>Sumitomo紹介</a></li>
+                    <li><a href="#"><i class ="fas fa-address-book"></i>お問い合わせ</a></li>
+                </ul>
+                <div class="search_group">
+                    <div class="search_buttons">
+                        <a href="#"><img src="../images/area.png" alt="エリアから探す" class="search_img1"></a>
+                    </div>
+                    <div class="search_buttons">
+                        <a href="#"><img src="../images/station.png" alt="エリアから探す" class="search_img2"></a>
+                    </div>
+                </div>
+                <div class="sign_in">
+                    <div class="login_buttons">
+                        <a href="#"><img src="../images/login.png" alt="login" class="login_img"></a>
+                        <a href="#"><img src="../images/join.png" alt="join" class="join_img"></a>
+                    </div>
+                </div>
+                <div class="social_media">
+                    <a href="#"><i class ="fab fa-facebook-square"></i></a>
+                    <a href="#"><i class ="fab fa-twitter"></i></a>
+                    <a href="#"><i class ="fab fa-instagram"></i></a>
+                </div>
+        </div>
+        <div class="main_content">
+            <div class="header">
+                헤더
+            </div>
+            <div class="info">
+                내용 타이틀
+                <div>
+                    이것은 내용입니다.
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/top.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.front_top')
+@section('title', 'Sumitomo')
+@section('content')
+    <div class="home_header">
+        <h2>部屋探しはここで！</h2>
+        <br><br><br>
+        <img src="../images/mainTitle.png" alt="main_title" class="main_title">
+        <br><br><br>
+        <a href="#"><img src="../images/search.jpg" alt="search" class="home_search"></a>
+    </div>
+@endsection
